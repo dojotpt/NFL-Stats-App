cadb0d89491a5d79837dc29b85273406074d155e
Don Johnson <donjohnsontpt@gmail.com>
2024-09-11 09:50:48 -0400

    modified DB. Set up Vue Files

--- java/src/Database/create.sh
+++ java/src/Database/create.sh
@@ -7,6 +7,10 @@ BASEDIR=$(dirname $0)
 # Define the database name
 DATABASE=fantasy_football
 
+# Define the path to your CSV file in the resources folder
+CSV_FILE="C:/Users/12706/IdeaProjects/FF2024/java/src/main/resources/pbp_data_2023.csv"
+
+
 # Drop the database if it exists
 dropdb --if-exists -U postgres $DATABASE
 
@@ -15,3 +19,6 @@ createdb -U postgres $DATABASE &&
 
 # Execute the schema.sql file to create tables
 psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql"
+
+# Populate the nfl_plays_2023 table from the CSV file
+psql -U postgres -d $DATABASE -c "\copy nfl_plays_2023 FROM '$CSV_FILE' DELIMITER ',' CSV HEADER"
