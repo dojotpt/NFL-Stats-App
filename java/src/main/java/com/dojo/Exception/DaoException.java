1947180b46926a0f7d0980a4ec7a4a12c7408778
Don Johnson <donjohnsontpt@gmail.com>
2024-09-12 14:13:30 -0400

    began models. created folders for dao,controllers, daoexceptions

---
+++ java/src/main/java/com/dojo/Exception/DaoException.java
@@ -0,0 +1,13 @@
+package com.dojo.Exception;
+
+public class DaoException extends RuntimeException {
+    public DaoException() {
+        super();
+    }
+    public DaoException(String message) {
+        super(message);
+    }
+    public DaoException(String message, Exception cause) {
+        super(message, cause);
+    }
+}
--- java/src/main/java/com/dojo/Models/PlayerStats.java
+++ java/src/main/java/com/dojo/Models/PlayerStats.java
@@ -2,5 +2,18 @@ package com.dojo.Models;
 
 public class PlayerStats {
 
-    public PlayerStats() {}
+    private String playerId;
+    private String playerName;
+    private String team;
+    private int season;
+    private int passYards;
+    private int rushYards;
+    private int receptions;
+    private int sacks;
+    private int touchdowns;
+
+
+public PlayerStats (){
+
+}
 }
