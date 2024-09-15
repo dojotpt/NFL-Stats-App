b2fa579051aefed503e870802f995badb1cde694
Don Johnson <donjohnsontpt@gmail.com>
2024-09-15 11:26:26 -0400

    added getQuarterbackStats and mapQBStatsToRowSet

--- java/src/main/java/com/dojo/DAO/JdbcQuarterbackStatsDao.java
+++ java/src/main/java/com/dojo/DAO/JdbcQuarterbackStatsDao.java
@@ -1,9 +1,14 @@
 package com.dojo.DAO;
 
+import com.dojo.Exception.DaoException;
 import com.dojo.Models.QuarterbackStats;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -13,6 +18,40 @@ public class JdbcQuarterbackStatsDao implements QuarterbackStatsDao {
     public JdbcQuarterbackStatsDao(JdbcTemplate jdbcTemplate) { this.jdbcTemplate = jdbcTemplate; }
     @Override
     public List<QuarterbackStats> getQuarterbackStats() {
-        return List.of();
+        final List<QuarterbackStats> quarterbackStats = new ArrayList<>();
+        final String sql = "SELECT player_id, player_name, team, season, pass_yards, pass_attempts, completions, pass_touchdowns, interceptions_thrown, sacks_taken, sack_yards_lost, " +
+                "pass_first_downs, yards_per_attempt, pass_20_plus, pass_40_plus, pass_longest\n" +
+                "\tFROM public.quarterback_stats;";
+
+        try {
+            final SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+            while (results.next()) {
+                quarterbackStats.add(mapRowToQuarterbackStats(results));
+            }
+        } catch (CannotGetJdbcConnectionException e) {
+                throw new DaoException("unable to connect to server or database", e);
+        }
+        return quarterbackStats;
+    }
+
+    QuarterbackStats mapRowToQuarterbackStats(SqlRowSet rowSet) {
+        QuarterbackStats quarterbackStats = new QuarterbackStats();
+        quarterbackStats.setPlayerId(rowSet.getString("player_id"));
+        quarterbackStats.setPlayerName(rowSet.getString("player_name"));
+        quarterbackStats.setTeam(rowSet.getString("team"));
+        quarterbackStats.setSeason(rowSet.getInt("season"));
+        quarterbackStats.setPassYards(rowSet.getInt("pass_yards"));
+        quarterbackStats.setPassAttempts(rowSet.getInt("pass_attempts"));
+        quarterbackStats.setCompletions(rowSet.getInt("completions"));
+        quarterbackStats.setPassTouchdowns(rowSet.getInt("pass_touchdowns"));
+        quarterbackStats.setInterceptions(rowSet.getInt("interceptions_thrown"));
+        quarterbackStats.setSacksTaken(rowSet.getInt("sacks_taken"));
+        quarterbackStats.setSackYardsLost(rowSet.getInt("sack_yards_lost"));
+        quarterbackStats.setPassFirstDowns(rowSet.getInt("pass_first_downs"));
+        quarterbackStats.setYardsPerAttempt(rowSet.getInt("yards_per_attempt"));
+        quarterbackStats.setPass20PlusYards(rowSet.getInt("pass_20_plus"));
+        quarterbackStats.setPass40PlusYards(rowSet.getInt("pass_40_plus"));
+        quarterbackStats.setPassLongest(rowSet.getInt("pass_longest"));
+        return quarterbackStats;
     }
 }
--- java/src/main/java/com/dojo/Models/QuarterbackStats.java
+++ java/src/main/java/com/dojo/Models/QuarterbackStats.java
@@ -5,20 +5,125 @@ public class QuarterbackStats {
     private String playerName;
     private String team;
     private int season;
+    private int passAttempts;
     private int passYards;
+    private int completions;
+    private int passTouchdowns;
+    private int Interceptions;
     private int rushYards;
-    private int sacked;
+    private int sacksTaken;
+    private int sackYardsLost;
+    private int passFirstDowns;
+    private int yardsPerAttempt;
+    private int pass20PlusYards;
+    private int pass40PlusYards;
+    private int passLongest;
     private int touchdowns;
 
-    public QuarterbackStats(String playerId, String playerName, String team, int season, int passYards, int rushYards, int sacked, int touchdowns) {
+    public QuarterbackStats(String playerId, String playerName, String team, int season, int passAttempts, int passYards, int completions, int passTouchdowns,
+                            int Interceptions, int rushYards, int sacksTaken, int sackYardsLost, int passFirstDowns, int yardsPerAttempt, int pass20PlusYards,
+                            int pass40PlusYards, int passLongest, int touchdowns) {
         this.playerId = playerId;
         this.playerName = playerName;
         this.team = team;
         this.season = season;
+        this.passAttempts = passAttempts;
         this.passYards = passYards;
+        this.completions = completions;
+        this.passTouchdowns = passTouchdowns;
+        this.Interceptions = Interceptions;
         this.rushYards = rushYards;
-        this.sacked = sacked;
+        this.sacksTaken = sacksTaken;
+        this.sackYardsLost = sackYardsLost;
+        this.passFirstDowns = passFirstDowns;
+        this.yardsPerAttempt = yardsPerAttempt;
+        this.pass20PlusYards = pass20PlusYards;
+        this.pass40PlusYards = pass40PlusYards;
+        this.passLongest = passLongest;
         this.touchdowns = touchdowns;
+    }
+
+
+    public QuarterbackStats() {}
+
+    public int getCompletions() {
+        return completions;
+    }
+
+    public void setCompletions(int completions) {
+        this.completions = completions;
+    }
+
+    public int getPassTouchdowns() {
+        return passTouchdowns;
+    }
+
+    public void setPassTouchdowns(int passTouchdowns) {
+        this.passTouchdowns = passTouchdowns;
+    }
+
+    public int getInterceptions() {
+        return Interceptions;
+    }
+
+    public void setInterceptions(int interceptions) {
+        Interceptions = interceptions;
+    }
+
+    public int getSacksTaken() {
+        return sacksTaken;
+    }
+
+    public void setSacksTaken(int sacksTaken) {
+        this.sacksTaken = sacksTaken;
+    }
+
+    public int getSackYardsLost() {
+        return sackYardsLost;
+    }
+
+    public void setSackYardsLost(int sackYardsLost) {
+        this.sackYardsLost = sackYardsLost;
+    }
+
+    public int getPassFirstDowns() {
+        return passFirstDowns;
+    }
+
+    public void setPassFirstDowns(int passFirstDowns) {
+        this.passFirstDowns = passFirstDowns;
+    }
+
+    public int getYardsPerAttempt() {
+        return yardsPerAttempt;
+    }
+
+    public void setYardsPerAttempt(int yardsPerAttempt) {
+        this.yardsPerAttempt = yardsPerAttempt;
+    }
+
+    public int getPass20PlusYards() {
+        return pass20PlusYards;
+    }
+
+    public void setPass20PlusYards(int pass20PlusYards) {
+        this.pass20PlusYards = pass20PlusYards;
+    }
+
+    public int getPass40PlusYards() {
+        return pass40PlusYards;
+    }
+
+    public void setPass40PlusYards(int pass40PlusYards) {
+        this.pass40PlusYards = pass40PlusYards;
+    }
+
+    public int getPassLongest() {
+        return passLongest;
+    }
+
+    public void setPassLongest(int passLongest) {
+        this.passLongest = passLongest;
     }
 
     public String getPlayerId() {
@@ -61,6 +166,14 @@ public class QuarterbackStats {
         this.passYards = passYards;
     }
 
+    public int getPassAttempts() {
+        return passAttempts;
+    }
+
+    public void setPassAttempts(int pass_attempts) {
+        this.passAttempts = pass_attempts;
+    }
+
     public int getRushYards() {
         return rushYards;
     }
@@ -70,11 +183,11 @@ public class QuarterbackStats {
     }
 
     public int getSacked() {
-        return sacked;
+        return sacksTaken;
     }
 
     public void setSacked(int sacked) {
-        this.sacked = sacked;
+        this.sacksTaken = sacked;
     }
 
     public int getTouchdowns() {
@@ -84,4 +197,5 @@ public class QuarterbackStats {
     public void setTouchdowns(int touchdowns) {
         this.touchdowns = touchdowns;
     }
+
 }
