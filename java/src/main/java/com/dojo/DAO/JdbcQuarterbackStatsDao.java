c5f3a676e75fe199353b4573e1f06a14a846e64b
Don Johnson <donjohnsontpt@gmail.com>
2024-09-14 20:17:31 -0400

    started JdbcQBStatsDao

--- java/src/main/java/com/dojo/DAO/JdbcQuarterbackStatsDao.java
+++ java/src/main/java/com/dojo/DAO/JdbcQuarterbackStatsDao.java
@@ -1,4 +1,18 @@
 package com.dojo.DAO;
 
-public class JdbcQuarterbackStatsDao {
+import com.dojo.Models.QuarterbackStats;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class JdbcQuarterbackStatsDao implements QuarterbackStatsDao {
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcQuarterbackStatsDao(JdbcTemplate jdbcTemplate) { this.jdbcTemplate = jdbcTemplate; }
+    @Override
+    public List<QuarterbackStats> getQuarterbackStats() {
+        return List.of();
+    }
 }
--- java/src/main/java/com/dojo/DAO/QuarterbackStatsDao.java
+++ java/src/main/java/com/dojo/DAO/QuarterbackStatsDao.java
@@ -1,4 +1,9 @@
 package com.dojo.DAO;
 
+import com.dojo.Models.QuarterbackStats;
+
+import java.util.List;
+
 public interface QuarterbackStatsDao {
+    List<QuarterbackStats> getQuarterbackStats();
 }
