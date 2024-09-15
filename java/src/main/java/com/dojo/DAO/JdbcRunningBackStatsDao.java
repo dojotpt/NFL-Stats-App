0979fae77e55aa7051643c5cb617e3efbe527745
Don Johnson <donjohnsontpt@gmail.com>
2024-09-14 20:19:21 -0400

    started rb stats dao

--- java/src/main/java/com/dojo/DAO/JdbcRunningBackStatsDao.java
+++ java/src/main/java/com/dojo/DAO/JdbcRunningBackStatsDao.java
@@ -1,4 +1,18 @@
 package com.dojo.DAO;
 
-public class JdbcRunningBackStatsDao {
+import com.dojo.Models.QuarterbackStats;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class JdbcRunningBackStatsDao implements RunningBackStatsDao {
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcRunningBackStatsDao(JdbcTemplate jdbcTemplate) { this.jdbcTemplate = jdbcTemplate; }
+    @Override
+    public List<QuarterbackStats> getQuarterbackStats() {
+        return List.of();
+    }
 }
--- java/src/main/java/com/dojo/DAO/RunningBackStatsDao.java
+++ java/src/main/java/com/dojo/DAO/RunningBackStatsDao.java
@@ -1,4 +1,9 @@
 package com.dojo.DAO;
 
+import com.dojo.Models.QuarterbackStats;
+
+import java.util.List;
+
 public interface RunningBackStatsDao {
+    List<QuarterbackStats> getQuarterbackStats();
 }
