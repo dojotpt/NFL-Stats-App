1246f7ec7d56150e5d8db73602d34a1d1f96cfe4
Don Johnson <donjohnsontpt@gmail.com>
2024-09-24 14:47:14 -0400

    Started tight end stats controller

---
+++ java/src/main/java/com/dojo/Controller/TightEndStatsController.java
@@ -0,0 +1,24 @@
+package com.dojo.Controller;
+
+import com.dojo.DAO.TightEndStatsDao;
+import com.dojo.Models.TightEndStats;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class TightEndStatsController {
+    private final TightEndStatsDao tightEndStatsDao;
+
+    public TightEndStatsController(TightEndStatsDao tightEndStatsDao) {
+        this.tightEndStatsDao = tightEndStatsDao;
+    }
+    @GetMapping("/stats/te")
+    public TightEndStatsResponseDTO getTightEndStats() {
+        final List<TightEndStats> tightEndStatsList = this.tightEndStatsDao.getTightEndStats();
+    return new TightEndStatsResponseDTO(tightEndStatsList);
+    }
+}
